1465cc076dcb742d4bc7f64b0e0726f927ee5ba1
Nelson Widjaya <nelson.widjaya@gmail.com>
2020-02-18 23:24:13 +0900

    install bootstrap test

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,9 @@
 // about supported directives.
 //
 //= require jquery
-//= require rails-ujs
+//= require jquery_ujs
 //= require bootstrap-sprockets
+//= require rails-ujs
 //= require activestorage
 //= require turbolinks
 //= require_tree .
